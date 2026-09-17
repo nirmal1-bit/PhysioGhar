1e7172f7ef6ed7e6f83c1e7c48545d0d9b9660d2
Nirmal Chhetri <chhetrinirmal765@gmail.com>
2026-09-17 23:42:48 +0545

    chore(config): point api client to production host

--- lib/core/providers/core_providers.dart
+++ lib/core/providers/core_providers.dart
@@ -11,7 +11,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 final dioProvider = Provider<Dio>((ref) {
   final dio = Dio(
     BaseOptions(
-      baseUrl: 'http://10.155.133.34:8000/api/v1',
+      baseUrl: 'https://prod.creativeinkflow.tech/api/v1',
       connectTimeout: const Duration(seconds: 15),
       receiveTimeout: const Duration(seconds: 15),
       headers: {'Content-Type': 'application/json'},
